d89cf835d8a483d5c1de5eaa950bf4e1c6606533
Eluan96 <eluan.ea@gmail.com>
2023-04-21 23:29:10 +0100

    solved

---
+++ divisibleby2and3.js
@@ -0,0 +1,16 @@
+// Divisble by 2 and 3
+
+function divisible(arr){
+    let length = 0;
+  
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] % 2 === 0 && arr[i] % 3 === 0 && arr[i] > 15){
+        length++
+      }
+    }
+    return length
+  }
+  
+  console.log(divisible([2,3,12,18,42,24])) //to return 3
+  console.log(divisible([100,84,22,37])) //to return 1
+  console.log(divisible([5,8,6,12])) //to return 0
