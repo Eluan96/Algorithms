e2ffa871776f9a631cbe1754ed92ed3124de4879
Eluan96 <eluan.ea@gmail.com>
2023-04-23 13:52:34 +0100

    solved

---
+++ disemvowels.js
@@ -0,0 +1,10 @@
+function disemvowel(str) {
+    let result = "";
+     
+     for(let i = 0; i < str.length; i++){
+       if (!"aeiouAEIOU".includes(str[i])){
+         result += str[i]
+       }
+     }
+     return result
+   }
