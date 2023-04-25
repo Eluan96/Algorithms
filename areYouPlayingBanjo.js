31bd5f0e781b3d05b6be33615081f57b36742844
BBS <bbs@BBSs-MacBook-Air.local>
2023-04-25 16:23:01 +0100

    solved

---
+++ areYouPlayingBanjo.js
@@ -0,0 +1,24 @@
+function areYouPlayingBanjo(name) {
+    const a = "plays banjo" 
+    const b = "does not play banjo"
+    let res = ""
+  
+    if(name[0] === "r" || name[0] === "R"){
+      res += name + " " + a
+    }
+    else {
+      res += name + " " + b
+    }
+    return res;
+  }
+
+  //OR
+
+  function areYouPlayingBanjo(name){
+    if(name[0].toLowerCase() == "r"){
+        return name + ' plays banjo';
+    }
+    else {
+        return name + ' does not play banjo'
+    }
+  }
