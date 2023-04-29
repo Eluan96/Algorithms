b8063ec4079ed71417580ca66d5464ba3328b995
BBS <bbs@BBSs-MacBook-Air.local>
2023-04-29 16:15:48 +0100

    solved

---
+++ getDivisorsCnt.js
@@ -0,0 +1,13 @@
+function getDivisorsCnt(n){
+    let count = 0;
+    for (let i = 1; i * i <= n; i++) {
+      if (n % i === 0) {
+        count++;
+      
+        if (i !== n/i) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
