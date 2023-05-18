3829914ecee7ebb07e27cc6c46c6842c84d70e5b
BBS <bbs@BBSs-MacBook-Air.local>
2023-05-18 01:24:49 +0100

    solved

---
+++ withdrawAmount.js
@@ -0,0 +1,24 @@
+function withdraw(amount){
+    let hundred = 0;
+    let fifty = 0;
+    let twenty = 0;
+  
+    while(amount >= 20){
+      if(amount % 100 === 0){
+      amount -= 100
+      hundred++
+      }
+      else if(amount % 50 === 0){
+        amount -= 50
+        fifty++
+      }
+      else {
+        amount -= 20
+        twenty++
+      }
+    }
+    return [hundred, fifty, twenty]
+  }
+  
+  console.log(withdraw(160))
+  
