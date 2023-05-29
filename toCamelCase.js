dc2cc19408360e3e57b411da3728339fa61e007e
BBS <bbs@BBSs-MacBook-Air.local>
2023-05-29 23:37:05 +0100

    solved

--- toCamelCase.js
+++ toCamelCase.js
@@ -1,9 +1,13 @@
 function toCamelCase(str){
     let words = str.split(/[-_]/)
+    console.log(words)
     for (let i = 0; i < words.length; i++) {
-        
+        words[i] = words[i].split('')
+        console.log(words[i])
+        words[i][0] = words[i][0].toUpperCase()
+        words[i] = words[i].join("")
     }
-    return words//.join('')
+    return words.join(" ")
 }
 console.log(toCamelCase('the-stealth-warrior'))
 console.log(toCamelCase('The_stealth_warrior'))
